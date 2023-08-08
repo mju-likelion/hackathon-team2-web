b890e8a0b264101d29325af81c928d30dcb0e982
constantly-dev <rntqhdl10@naver.com>
2023-08-08 21:43:45 +0900

    feat: mainheader 구현

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import GlobalStyle from './styles/GlobalStyles';
 import { ThemeProvider } from 'styled-components';
+
+import MainHeader from './components/MainHeader.jsx';
+import GlobalStyle from './styles/GlobalStyles';
 import { Theme } from './styles/Theme';
 
 const App = () => {
   return (
     <ThemeProvider theme={Theme}>
       <GlobalStyle />
-      <></>
+      <MainHeader></MainHeader>
     </ThemeProvider>
   );
 };
---
+++ src/components/MainHeader.jsx
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+
+import { ReactComponent as LogoIcon } from '../assets/images/logo icon.svg';
+
+const MainHeader = () => {
+  return (
+    <Container>
+      <TitleWrapper>
+        <Title>모잇</Title>
+        <LogoIcon></LogoIcon>
+      </TitleWrapper>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  width: 100%;
+  height: 46px;
+  padding: 5px 30px 5px 30px;
+  border-bottom-left-radius: 12px;
+  border-bottom-right-radius: 12px;
+  background-color: ${({ theme }) => theme.colors.YELLOW};
+`;
+const TitleWrapper = styled.div`
+  width: 82px;
+  height: 36px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+const Title = styled.div`
+  height: 36px;
+  font-size: 24px;
+  font-weight: 900;
+  color: #ffffff;
+  display: flex;
+  align-items: center;
+`;
+
+export default MainHeader;
