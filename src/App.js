fb70f4bed15af786649af76095058ec5c384f7de
constantly-dev <rntqhdl10@naver.com>
2023-08-09 16:17:03 +0900

    feat: maintooltip 구현

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { ThemeProvider } from 'styled-components';
 
+import MainToolTip from './components/MainToolTip.jsx';
 import GlobalStyle from './styles/GlobalStyles';
 import { Theme } from './styles/Theme';
-
 const App = () => {
   return (
     <ThemeProvider theme={Theme}>
       <GlobalStyle />
+      <MainToolTip />
     </ThemeProvider>
   );
 };
