2dcc462c38cfb1b25a91d531faa31c3b29e22e19
kimdongyoung <wwweric12@naver.com>
2023-08-09 18:58:10 +0900

    feat: link-component 구현

---
+++ src/components/LinkComponent.jsx
@@ -0,0 +1,27 @@
+import { styled } from 'styled-components';
+
+import Next from '../assets/images/Next.jsx';
+
+const LinkComponent = ({ text, color }) => {
+  return (
+    <LinkContainer color={color}>
+      <LinkText>{text}</LinkText>
+      <Next color={color} />
+    </LinkContainer>
+  );
+};
+
+export default LinkComponent;
+
+const LinkContainer = styled.div`
+  display: flex;
+  align-items: end;
+  color: ${(props) => props.color};
+`;
+
+const LinkText = styled.div`
+  font-size: 13px;
+  font-weight: 500;
+  line-height: 18px;
+  margin-right: 2px;
+`;
