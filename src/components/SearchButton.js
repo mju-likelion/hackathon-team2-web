a2eecec3f030063520a6918f117c9e22886f09a2
constantly-dev <rntqhdl10@naver.com>
2023-08-11 18:16:38 +0900

    feat: errorpage 구현

--- src/components/SearchButton.js
+++ src/components/SearchButton.js
@@ -2,19 +2,19 @@ import { styled } from 'styled-components';
 
 import { ReactComponent as Research } from '../assets/images/research.svg';
 
-const ResearchButton = ({ bgColor, fontColor }) => {
-  const MarkerResearch = () => {}; // 마커 데이터 재호출
-
-  return (
-    <ButtonContainer onClick={MarkerResearch} bgColor={bgColor}>
-      {bgColor === '#FFE070' ? (
-        <ContentWrapper>
-          <Research />
-          <ResearchText fontColor={fontColor}>이 지역에서 다시 검색</ResearchText>
-        </ContentWrapper>
-      ) : (
-        <ResearchText fontColor={fontColor}>이 지역에서 다시 검색</ResearchText>
-      )}
+const SearchButton = ({ bgColor, fontColor }) => {
+  const markerResearch = () => {}; // 마커 데이터 재호출
+  const goBack = () => {};
+  return bgColor === '#FFE070' ? (
+    <ButtonContainer onClick={markerResearch} bgColor={bgColor}>
+      <ContentWrapper>
+        <Research />
+        <ButtonText fontColor={fontColor}>이 지역에서 다시 검색</ButtonText>
+      </ContentWrapper>
+    </ButtonContainer>
+  ) : (
+    <ButtonContainer onClick={goBack} bgColor={bgColor}>
+      <ButtonText fontColor={fontColor}>이전 페이지로 돌아가기</ButtonText>
     </ButtonContainer>
   );
 };
@@ -36,9 +36,7 @@ const ContentWrapper = styled.div`
   justify-content: space-between;
   align-items: center;
 `;
-const ResearchText = styled.p`
-  width: 117px;
-  height: 18px;
+const ButtonText = styled.p`
   display: flex;
   align-items: center;
   font-size: 13px;
@@ -46,4 +44,4 @@ const ResearchText = styled.p`
   line-height: 18px;
   color: ${({ fontColor }) => fontColor};
 `;
-export default ResearchButton;
+export default SearchButton;
