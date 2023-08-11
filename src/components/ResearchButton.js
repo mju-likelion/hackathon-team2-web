ed6ac1c26a5246a96881c71efbdd3c99dae5cb32
constantly-dev <rntqhdl10@naver.com>
2023-08-11 16:26:39 +0900

    chore: research button 수정

--- src/components/ResearchButton.js
+++ src/components/ResearchButton.js
@@ -2,24 +2,31 @@ import { styled } from 'styled-components';
 
 import { ReactComponent as Research } from '../assets/images/research.svg';
 
-const ResearchButton = () => {
+const ResearchButton = ({ bgColor, fontColor }) => {
   const MarkerResearch = () => {}; // 마커 데이터 재호출
 
   return (
-    <ButtonContainer onClick={MarkerResearch}>
-      <ContentWrapper>
-        <Research />
-        <ResearchText>이 지역에서 다시 검색</ResearchText>
-      </ContentWrapper>
+    <ButtonContainer onClick={MarkerResearch} bgColor={bgColor}>
+      {bgColor === '#FFE070' ? (
+        <ContentWrapper>
+          <Research />
+          <ResearchText fontColor={fontColor}>이 지역에서 다시 검색</ResearchText>
+        </ContentWrapper>
+      ) : (
+        <ResearchText fontColor={fontColor}>이 지역에서 다시 검색</ResearchText>
+      )}
     </ButtonContainer>
   );
 };
 
 const ButtonContainer = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
   width: 162px;
   height: 44px;
   padding: 10px 8px;
-  background-color: ${({ theme }) => theme.colors.YELLOW};
+  background-color: ${({ bgColor }) => bgColor};
   border-radius: 999px;
 `;
 const ContentWrapper = styled.div`
@@ -32,8 +39,11 @@ const ContentWrapper = styled.div`
 const ResearchText = styled.p`
   width: 117px;
   height: 18px;
+  display: flex;
+  align-items: center;
   font-size: 13px;
   font-weight: 500;
   line-height: 18px;
+  color: ${({ fontColor }) => fontColor};
 `;
 export default ResearchButton;
