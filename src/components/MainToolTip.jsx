bfc54510c788ee8dc7e8c689205c977fb1080727
constantly-dev <rntqhdl10@naver.com>
2023-08-13 01:17:19 +0900

    chore: textbox width값 수정

--- src/components/MainToolTip.jsx
+++ src/components/MainToolTip.jsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 
 const MainToolTip = () => {
   const [selectData, setSelectData] = useState('');
-  const toolTipData = ['오늘은 뭐 먹을까?', '어디서 뭐 먹지?', '베고파아아아~'];
+  const toolTipData = ['오늘은 뭐 먹을까?', '어디서 뭐 먹지?', '배고파아아아~'];
   useEffect(() => {
     const ranIndex = Math.floor(Math.random() * 3);
     setSelectData(toolTipData[ranIndex]);
@@ -28,8 +28,7 @@ const Container = styled.div`
 `;
 const Body = styled.div`
   width: 108px;
-  height: 46px;
-  padding: 14px 8px;
+  padding: 14px 4px;
   position: relative;
   border-radius: 999px;
   background-color: #ffffff;
@@ -44,6 +43,7 @@ const Arrow = styled.div`
   border-top: 6px solid #ffffff;
 `;
 const TextBox = styled.p`
+  width: 100px;
   text-align: center;
   ${({ theme }) => theme.typographies.body12}
 `;
